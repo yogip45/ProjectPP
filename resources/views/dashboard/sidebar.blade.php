9fb96ac8bac4b1d62b9337603d94007dcc3341fe
rere430 <cahyaningtyasregina@gmail.com>
2023-06-13 19:01:28 +0700

    cumq ilangin menu satu yg gakepake

--- resources/views/dashboard/sidebar.blade.php
+++ resources/views/dashboard/sidebar.blade.php
@@ -111,12 +111,12 @@
         </a>
       </div>
     </li>
-    <li class="nav-item sidebar-user-actions">
+    <!-- <li class="nav-item sidebar-user-actions">
       <div class="sidebar-user-menu">
         <a href="#" class="nav-link"><i class="mdi mdi-speedometer menu-icon"></i>
           <span class="menu-title">Take Tour</span></a>
       </div>
-    </li>
+    </li> -->
     <li class="nav-item sidebar-user-actions">
       <div class="sidebar-user-menu">
         <a href="/sesi/logout" class="nav-link"><i class="mdi mdi-logout menu-icon"></i>
